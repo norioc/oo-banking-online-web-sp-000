12586b76e72a6802a162968723922070690b5279
norio c <norionorioc@gmail.com>
2020-04-24 21:59:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,27 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :status
+  attr_reader :amount, :sender, :receiver 
+
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver 
+    @amount = 50 
+  end 
+  
+  def valid? 
+    sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction 
+    
+    
+    
+  end
+  
+  
+  
+  
+  
 end
